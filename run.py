1a4e4d0eb105d59175026cbde3f1ca7855a370de
Ryan <ryan.pleava@gmail.com>
2022-12-03 14:36:04 -0500

    Started adding constraints for guesses

--- run.py
+++ run.py
@@ -1,28 +1,31 @@
 import pprint
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
+from itertools import permutations
 
 # Encoding that will store all of your constraints
 E = Encoding()
 
 COL_COUNT = 4
-ROW_COUNT = 11
+ROW_COUNT = 5
 
 # Creates an 11x4 matrix of 0s
 empty_board = [[0]*COL_COUNT for _ in range(ROW_COUNT)]
 
-COLOURS = ["purple", "red", "green", "yellow", "teal"]
+COLOURS = ["purple", "red", "green", "yellow", "teal", "orange"]
 COLS = range(COL_COUNT)
 ROWS = range(ROW_COUNT)
 
 
-@proposition(E)
+
 class Base:
     def __init__(self, col: int, row: int):
         self.col = col
         self.row = row
 
 
+# Proposition for a peg of a particular color in a particular position
+@proposition(E)
 class ColourPos(Base):
 
     def __init__(self,  col: int, row: int, colour: str):
@@ -36,17 +39,17 @@ class ColourPos(Base):
 class Feedback(Base):
     pass
 
-
+@proposition(E)
 class WhiteFeedbackPos(Feedback):
     def __repr__(self):
         return f"White Feedback @ ({self.row}, {self.col})"
 
-
+@proposition(E)
 class BlackFeedbackPos(Feedback):
     def __repr__(self):
         return f"Black Feedback @ ({self.row}, {self.col})"
 
-
+@proposition(E)
 class EmptyFeedbackPos(Feedback):
     def __repr__(self):
         return f"Empty Feedback @ ({self.row}, {self.col})"
@@ -59,6 +62,8 @@ for row in ROWS:
             E, [ColourPos(colour, col, row) for colour in COLOURS])
 
 
+
+
 # Each feedback can only have ONE type of feedback (white, black empty)
 for row in ROWS:
     for col in COLS:
@@ -66,6 +71,33 @@ for row in ROWS:
             E, [WhiteFeedbackPos(col, row),
                 EmptyFeedbackPos(col, row),
                 BlackFeedbackPos(col, row)])
+
+
+
+
+
+# Proposition for guesses
+@proposition(E)
+class Guess:
+    def __init__(self, val):
+        self.val = val
+    def __repr__(self):
+        return f"Guess: {' '.join(self.val)}"
+
+        
+
+# Generate all valid guesses (no duplicate colours per guess)
+all_valid_guesses = permutations(COLOURS, 4) 
+
+
+# Each guess must must not contain duplicate colours
+for row in ROWS:
+    constraint.add_exactly_one(E, [Guess(val) for val in all_valid_guesses])
+
+# Guesses and answer cannot contains duplicate colours.
+# for row in ROWS:
+#     for col in COLS:
+        
 
 
 # Build an example full theory for your setting and return it.
@@ -91,10 +123,12 @@ if __name__ == "__main__":
     print("\nSatisfiable: %s" % T.satisfiable())
     print("# Solutions: %d" % count_solutions(T))
     print("   Solution: %s" % T.solve())
-
+    print("Len is:", len(list(all_valid_guesses)))
     print("\nVariable likelihoods:")
     for v,vn in zip([a, b, c, x, y, z], 'abcxyz'):
         # Ensure that you only send these functions NNF formulas
         # Literals are compiled to NNF here
         print(" %s: %.2f" % (vn, likelihood(T, v)))
     print()
+
+    
---
+++ rundocker.sh
@@ -0,0 +1,4 @@
+#!bin/bash/
+
+sudo docker run -l mastermind-run -it --hostname muffin --rm -v /home/muffin/CISC_204/mastermind:/PROJECT mastermind-img
+
